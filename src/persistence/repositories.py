c453fa927e3c541967cf13b460f6ba78d1fa9398
Earthquakoo <cream5343@gmail.com>
2023-10-16 17:54:54 +0900

    Update first version

--- src/persistence/repositories.py
+++ src/persistence/repositories.py
@@ -4,7 +4,6 @@ import src.persistence.models as models
 
 
 def delete_alarm(db: Session, alarm_obj):
-
     db.delete(alarm_obj)
     db.commit()
     
@@ -12,7 +11,6 @@ def delete_alarm(db: Session, alarm_obj):
 
 
 def repeat_schedule_message(db: Session, repeat_alarm: dict, alarm_obj):
-    
     setattr(alarm_obj, 'alarm_date', repeat_alarm['alarm_date'])
     setattr(alarm_obj, 'scheduled_message_id', repeat_alarm['scheduled_message_id'])
     db.commit()
@@ -21,7 +19,6 @@ def repeat_schedule_message(db: Session, repeat_alarm: dict, alarm_obj):
 
 
 def bot_info_init(db: Session, bot_info: dict):
-    
     bot_info_init = models.User(**bot_info)
     
     db.add(bot_info_init)
